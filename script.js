cc6b3e6c9bdff263613d4f8cb031e64af29424d4
Thomas OvO <thonnad.thomas@gmail.com>
2023-09-17 01:53:36 +0200

    Todo-List

---
+++ script.js
@@ -0,0 +1,32 @@
+const form = document.querySelector("form");
+
+function storeList() {
+  window.localStorage.todoList = list.innerHTML
+}
+
+function getTodos() {
+  if (window.localStorage.todoList) {
+    list.innerHTML = window.localStorage.todoList;
+  } else {
+    list.innerHTML = `<li>Cliquez sur un Todo pour le supprimer</li>`
+  }
+}
+
+getTodos();
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  list.innerHTML += `<li>${item.value}</li>`;
+  item.value = "";
+  storeList();
+});
+
+list.addEventListener("click", (e) => {
+  if (e.target.classList.contains("checked")) {
+    e.target.remove();
+  } else {
+    e.target.classList.add("checked");
+  }
+  storeList();
+});
